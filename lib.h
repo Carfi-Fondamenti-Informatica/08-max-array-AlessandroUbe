215f53357b1b59b0b7e1fa3db76c21f82c4185ac
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-12-13 09:47:47 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+//
+// Created by Alessandro Ubertalli on 09/12/2022.
+//
 
+#ifndef ESERCITAZIONE_8_LIB_H
+#define ESERCITAZIONE_8_LIB_H
+float array(float a[], int d);
+#endif //ESERCITAZIONE_8_LIB_H
