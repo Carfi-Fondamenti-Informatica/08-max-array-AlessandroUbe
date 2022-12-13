d523bbef6e725e08d5f625e5ff4849113be7aba7
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-12-13 09:49:55 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,13 +3,13 @@
 //
 
 #include "lib.h"
-float array(float a[], int d){
-    for(int i=0;i<d;i++){
-        if(a[0]<=a[i]){
+float array(float a[], int  d){
+    for (int i=0;i<d;i++){
+        if (a[0]<=a[i]){
             a[0]=a[i];
         }else{
             a[i]=a[0];
         }
     }
-    return a[0];
+    return  a[0];
 }
